86f44fdd4c018817673cc282f2833fc86e6d22b0
BAD-99 <brandon.andrew.davis@gmail.com>
2023-12-01 10:23:39 -0700

    removed last element

--- 2023/1.js
+++ 2023/1.js
@@ -1,4 +1,5 @@
-const lineList = document.querySelector('pre').innerText.replace([a-zA-Z], '').split(' ');
-const numberList = lineList.map(line => line.slice(0) + line.slice(-1));
-const sum = numberList.reduce((acc, val) => acc + parseInt(val), 0);
+const lineList = document.querySelector('pre').innerText.replace(/[a-zA-Z]+/g, '').split(`\n`);
+lineList.pop();
+const numberList = lineList.map(line => parseInt(line.slice(0,1) + line.slice(-1)));
+const sum = numberList.reduce((acc, val) => acc + val,0);
 console.log(sum);
