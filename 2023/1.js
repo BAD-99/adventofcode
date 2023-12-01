7576198d375902189bfe9c5d29521db0c677a12e
BAD-99 <brandon.andrew.davis@gmail.com>
2023-12-01 11:19:44 -0700

    adjusted formatting

--- 2023/1.js
+++ 2023/1.js
@@ -1,5 +1,7 @@
-const lineList = document.querySelector('pre').innerText.replace(/[a-zA-Z]+/g, '').split(`\n`);
+const lineList = document.querySelector('pre').innerText
+    .replace(/[a-zA-Z]+/g, '')
+    .split(`\n`);
 lineList.pop();
-const numberList = lineList.map(line => parseInt(line.slice(0,1) + line.slice(-1)));
-const sum = numberList.reduce((acc, val) => acc + val,0);
+const numberList = lineList.map(line => parseInt(line.slice(0, 1) + line.slice(-1)));
+const sum = numberList.reduce((acc, val) => acc + val);
 console.log(sum);
--- 2023/2.js
+++ 2023/2.js
@@ -1,8 +1,9 @@
 const replaceArray = [['one', 1], ['two', 2], ['three', 3], ['four', 4], ['five', 5], ['six', 6], ['seven', 7], ['eight', 8], ['nine', 9]];
-const lineList = replaceArray.reduce((a, b) => a.replaceAll(b[0], b[0] + b[1] + b[0]), document.querySelector('pre').innerText).replace(/[a-zA-Z]+/g, '').split('\n');
+const lineList = replaceArray
+    .reduce((a, b) => a.replaceAll(b[0], b[0] + b[1] + b[0]), document.querySelector('pre').innerText)
+    .replace(/[a-zA-Z]+/g, '')
+    .split('\n');
 lineList.pop();
-const numberList = lineList.map(line => {
-    return parseInt(line.slice(0, 1) + line.slice(-1));
-});
-const sum = numberList.reduce((acc, val) => acc + val, 0);
+const numberList = lineList.map(line => parseInt(line.slice(0, 1) + line.slice(-1)));
+const sum = numberList.reduce((acc, val) => acc + val);
 console.log(sum);
